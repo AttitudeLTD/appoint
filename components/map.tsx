578a888ce30e310d3061e3da313f38575fedcad0
Egidio Salinaro <egy.s96@gmail.com>
2024-09-05 13:07:21 +0000

    Sep 5, 2024, 3:07 PM

--- components/map.tsx
+++ components/map.tsx
@@ -17,10 +17,7 @@ const Map = () => {
       zoom={13}
       scrollWheelZoom={true}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
+      <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
       <Marker
         icon={
           new L.Icon({
