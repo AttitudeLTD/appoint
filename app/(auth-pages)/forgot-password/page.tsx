9d1efd15cc44ad7ebb2444fc72cc7da6588a3e63
Egidio Salinaro <egy.s96@gmail.com>
2024-09-05 13:39:38 +0000

    Sep 5, 2024, 3:39 PM

--- app/(auth-pages)/forgot-password/page.tsx
+++ app/(auth-pages)/forgot-password/page.tsx
@@ -13,7 +13,7 @@ export default function ForgotPassword({
 }) {
   return (
     <>
-      <form className='flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto'>
+      <form className='my-20 flex-1 flex flex-col w-full gap-2 text-foreground [&>input]:mb-6 min-w-64 max-w-64 mx-auto'>
         <div>
           <h1 className='text-2xl font-medium'>Reset Password</h1>
           <p className='text-sm text-secondary-foreground'>
