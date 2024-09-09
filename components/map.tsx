87207aa42c7c2affbd725a404c47f2dec12755e1
Egidio Salinaro <egy.s96@gmail.com>
2024-09-09 13:19:35 +0000

    Sep 9, 2024, 3:19 PM

--- components/map.tsx
+++ components/map.tsx
@@ -1,21 +1,34 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { createClient } from '@/utils/supabase/client';
 
 import { LatLngExpression } from 'leaflet';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 import { customNavigationIcon, getMyLocation } from '@/utils/navigation';
-import { createClient } from '@/utils/supabase/client';
 
 const Map = ({ user }: any) => {
   const supabase = createClient();
+
+  const [userName, setUserName] = useState<string | null>(null);
   const [coord, setCoord] = useState<LatLngExpression | null>(null);
 
   useEffect(() => {
+    const getUserName = async () => {
+      const { data: userName } = await supabase
+        .from('users')
+        .select('name')
+        .eq('id', user.id)
+        .single();
+
+      setUserName(userName?.name || null);
+    };
+
+    getUserName();
     getMyLocation(setCoord);
-  }, [supabase]);
+  }, [user.id, supabase]);
 
   return (
     <>
@@ -32,7 +45,7 @@ const Map = ({ user }: any) => {
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
           <Marker icon={customNavigationIcon} position={coord}>
             <Popup>
-              Ciao {user.id}, oggi ti mancano 3 attività per raggiungere il tuo
+              Ciao {userName}, oggi ti mancano 3 attività per raggiungere il tuo
               obiettivo.
             </Popup>
           </Marker>
