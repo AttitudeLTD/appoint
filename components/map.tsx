4885052f352b9c550af94cc1c959986209d2d650
Egidio Salinaro <egy.s96@gmail.com>
2024-09-09 11:08:31 +0000

    Sep 9, 2024, 1:08 PM

--- components/map.tsx
+++ components/map.tsx
@@ -30,7 +30,7 @@ const Map = () => {
           <TileLayer url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' />
           <Marker icon={customNavigationIcon} position={coord}>
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              A pretty CSS3 popup! <br /> Easily customizable.
             </Popup>
           </Marker>
         </MapContainer>
