d948a5e1ca46bb8bd8d44d08dc794e013d467329
Egidio Salinaro <egy.s96@gmail.com>
2024-09-09 14:30:39 +0000

    Sep 9, 2024, 4:30 PM

--- components/map.tsx
+++ components/map.tsx
@@ -12,6 +12,7 @@ import {
   getMyLocation,
   parseCoords,
   storeIcon,
+  haversineDistance,
 } from '@/utils/navigation';
 
 interface Store {
@@ -26,31 +27,6 @@ const Map = ({ user }: any) => {
   const [userName, setUserName] = useState<string | null>(null);
   const [coord, setCoord] = useState<LatLngExpression | null>(null);
   const [stores, setStores] = useState<Store[]>([]);
-
-  const haversineDistance = (
-    coords1: LatLngExpression,
-    coords2: LatLngExpression
-  ): number => {
-    const toRad = (x: number) => (x * Math.PI) / 180;
-    const R = 6371; // Radius of the Earth in km
-
-    const [lat1, lon1] = coords1 as [number, number];
-    const [lat2, lon2] = coords2 as [number, number];
-
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return R * c; // Distance in km
-  };
 
   useEffect(() => {
     // Function to get the username of the current user
