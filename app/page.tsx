cc22edc3c993ea5a761612f6d2b2e8a11d92dd03
Egidio Salinaro <egy.s96@gmail.com>
2024-09-11 10:48:30 +0000

    Sep 11, 2024, 12:48 PM

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default async function Index() {
-  return <>ciao </>;
+  return (
+    <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden'>
+      <div></div>
+    </main>
+  );
 }
