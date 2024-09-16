c7e724796abb41eee0826b9fea4680cb640fc9a1
Egidio Salinaro <egy.s96@gmail.com>
2024-09-16 08:45:20 +0000

    Sep 16, 2024, 10:45 AM

--- components/hero.tsx
+++ components/hero.tsx
@@ -32,7 +32,7 @@ const Hero = () => {
             words='Trova la prossima attività sul territorio'
           />
 
-          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+          <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg'>
             Organizza le tue attività di vendita in base al miglior percorso
           </p>
         </div>
