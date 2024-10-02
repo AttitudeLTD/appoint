d38c818d15c527d93b5895970681e10f19c3ca01
egidiosalinaro <egy.s96@gmail.com>
2024-10-02 14:06:13 +0000

    2 ott 2024, 16:06

--- data/index.ts
+++ data/index.ts
@@ -38,7 +38,7 @@ export const gridItems = [
   },
   {
     id: 4,
-    title: 'Tech enthusiast with a passion for development.',
+    title: 'Raggiungi ogni cliente, ovunque si trovi.',
     description: '',
     className: 'lg:col-span-2 md:col-span-3 md:row-span-1',
     imgClassName: '',
@@ -49,8 +49,9 @@ export const gridItems = [
 
   {
     id: 5,
-    title: 'Currently building a JS Animation library',
-    description: 'The Inside Scoop',
+    title:
+      'Tecnologia avanzata di gestione del territorio per risultati straordinari.',
+    description: 'Ogni metro conta per crescere.',
     className: 'md:col-span-3 md:row-span-2',
     imgClassName: 'absolute right-0 bottom-0 md:w-96 w-60',
     titleClassName: 'justify-center md:justify-start lg:justify-center',
