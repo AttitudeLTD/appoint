9847874e362c74af01e4166a6cc0d4e12aeaad69
egidiosalinaro <egy.s96@gmail.com>
2024-10-02 14:01:56 +0000

    2 ott 2024, 16:01

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -52,8 +52,8 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ['ReactJS', 'Express', 'Typescript'];
-  const rightLists = ['VueJS', 'NuxtJS', 'GraphQL'];
+  const leftLists = ['Negozi', 'Ristoranti', 'Convenzionamenti'];
+  const rightLists = ['Attivazioni', 'Aziende', 'Vendite'];
 
   const [copied, setCopied] = useState(false);
 
