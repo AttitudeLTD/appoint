f3ee0266062304985aaf7f30bead8452491ea532
egidiosalinaro <egy.s96@gmail.com>
2024-10-02 10:14:10 +0000

    2 ott 2024, 12:14

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { FaHome } from 'react-icons/fa';
+import { navItems } from '@/data';
 
 import Hero from '@/components/hero';
 import Grid from '@/components/grid';
@@ -9,9 +9,7 @@ export default async function Index() {
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
-        <FloatingNav
-          navItems={[{ name: 'Home', link: '/', icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
       </div>
