1f9eabbbfc56d482396bb10630a2033326761e51
egidiosalinaro <egy.s96@gmail.com>
2024-10-02 14:07:26 +0000

    2 ott 2024, 16:07

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -186,7 +186,7 @@ export const BentoGridItem = ({
               </div>
 
               <MagicButton
-                title={copied ? 'Email is Copied!' : 'Copy my email address'}
+                title={copied ? 'Email copiata!' : 'Scrivici'}
                 icon={<IoCopyOutline />}
                 position='left'
                 handleClick={handleCopy}
