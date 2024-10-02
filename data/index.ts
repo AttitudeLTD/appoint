8d52ae59c8636102357cb2514c94ca23259c2dbd
egidiosalinaro <egy.s96@gmail.com>
2024-10-02 13:59:25 +0000

    2 ott 2024, 15:59

--- data/index.ts
+++ data/index.ts
@@ -18,7 +18,7 @@ export const gridItems = [
   },
   {
     id: 2,
-    title: "I'm very flexible with time zone communications",
+    title: 'Ottimizza a monte i tuoi spostamenti prima ancora di partire',
     description: '',
     className: 'lg:col-span-2 md:col-span-3 md:row-span-2',
     imgClassName: '',
