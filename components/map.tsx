5c2f5b6b9bdc08fe3e974d59093e526584447ba4
Egidio Salinaro <egy.s96@gmail.com>
2024-10-04 13:55:42 +0000

    Oct 4, 2024, 3:55 PM

--- components/map.tsx
+++ components/map.tsx
@@ -65,7 +65,7 @@ const Map = ({ user }: any) => {
     // Fetch stores within 3km of user's location
     const getStoresWithinRadius = async (userLat: number, userLng: number) => {
       const { data: storeData, error } = await supabase.rpc(
-        'get_stores_within_radius',
+        'getstoreswithinradius',
         {
           lat: userLat,
           lng: userLng,
