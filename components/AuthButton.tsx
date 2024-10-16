a3f17a22b9bf5840e132ed5c105ee02884cfdb39
Egidio Salinaro <egy.s96@gmail.com>
2024-10-16 13:44:08 +0000

    Oct 16, 2024, 3:44 PM

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -9,6 +9,12 @@ export default async function AuthButton() {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const { data: userName, error } = await supabase
+    .from('users')
+    .select('name')
+    .eq('id', user?.id)
+    .single();
+
   const signOut = async () => {
     'use server';
 
@@ -17,9 +23,9 @@ export default async function AuthButton() {
     return redirect('/login');
   };
 
-  return user ? (
+  return userName ? (
     <div className='flex items-center gap-4'>
-      Ciao, {user.email}!
+      Ciao, {userName.name}!
       <form action={signOut}>
         <button className='py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover'>
           Logout
