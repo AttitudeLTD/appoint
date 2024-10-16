60036d77ae4ba7a3178b0ae23642cbb3be3164df
Egidio Salinaro <egy.s96@gmail.com>
2024-10-16 14:26:36 +0000

    Oct 16, 2024, 4:26 PM

--- utils/navigation.ts
+++ utils/navigation.ts
@@ -37,7 +37,7 @@ export const freeStoreIcon = new L.Icon({
 
 // In Progress Store Icon (Yellow)
 export const inProgressStoreSvgString = `
-<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-minus-inside"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><path d="M9 10h6"/></svg>`;
+<svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 24 24" fill="#ffbb00" stroke="white" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-map-pin-minus-inside"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><path d="M9 10h6"/></svg>`;
 export const inProgressStoreIconUrl = svgToDataUrl(inProgressStoreSvgString);
 export const inProgressStoreIcon = new L.Icon({
   iconUrl: inProgressStoreIconUrl,
