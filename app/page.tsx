a0171308988e59b0f22bd20758dcab95ad90de62
egidiosalinaro <egy.s96@gmail.com>
2024-10-25 11:59:55 +0000

    25 ott 2024, 13:59

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { FloatingNav } from '@/components/ui/floating-nav';
 
 export default async function Index() {
   return (
-    <main className='relative bg-gradient-to-r from-[#1B304E] to-[#264E85] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
+    <main className='relative bg-gradient-to-r from-[#264E85] to-[#1B304E] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
         {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
