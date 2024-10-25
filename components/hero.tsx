0a5c711b7ba9fc2347b07b010b43ed1bc37e07a5
Egidio Salinaro <egy.s96@gmail.com>
2024-10-25 13:27:13 +0000

    Oct 25, 2024, 3:27 PM

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,10 +22,10 @@ const Hero = () => {
         />
       </div>
 
-      <div className='h-[calc(100vh-460px)] w-full bg-transparent bg-grid-white/[0.03] absolute top-0 left-0 flex items-center justify-center'>
-        {/* Radial gradient for the container to give a faded look */}
-        {/* <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' /> */}
-      </div>
+      {/* <div className='h-[calc(100vh-460px)] w-full bg-transparent bg-grid-white/[0.03] absolute top-0 left-0 flex items-center justify-center'>
+        Radial gradient for the container to give a faded look
+        <div className='absolute pointer-events-none inset-0 flex items-center justify-center bg-black-100 [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
+      </div> */}
 
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
