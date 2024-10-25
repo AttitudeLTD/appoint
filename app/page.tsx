d8da8878459299e32715ad770fc2e01891c1186b
Egidio Salinaro <egy.s96@gmail.com>
2024-10-25 12:16:53 +0000

    Oct 25, 2024, 2:16 PM

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Index() {
   return (
     <main className='relative bg-[#1B304E] flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
-        <FloatingNav navItems={navItems} />
+        {/* <FloatingNav navItems={navItems} /> */}
         <Hero />
         <Grid />
       </div>
