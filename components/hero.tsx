c4e92102ab49d8e3797e9d5a757360b2c9b43825
egidiosalinaro <egy.s96@gmail.com>
2024-10-25 11:32:04 +0000

    25 ott 2024, 13:32

--- components/hero.tsx
+++ components/hero.tsx
@@ -22,7 +22,7 @@ const Hero = () => {
         />
       </div>
 
-      <div className='h-[calc(100vh-460px)] w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center'>
+      <div className='h-[calc(100vh-460px)] w-full bg-black-100 bg-grid-white/[0.03] absolute top-0 left-0 flex items-center justify-center'>
         {/* Radial gradient for the container to give a faded look */}
         <div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]' />
       </div>
