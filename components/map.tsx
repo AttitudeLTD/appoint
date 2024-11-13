53dd3a4eeefae4444e43bdcdf6028e9a7b37d54c
Egidio Salinaro <egy.s96@gmail.com>
2024-11-13 13:38:59 +0000

    Nov 13, 2024, 2:38 PM

--- components/map.tsx
+++ components/map.tsx
@@ -391,7 +391,7 @@ const Map = ({ user }: any) => {
                   {searchResults.map((result, index) => (
                     <Button
                       key={index}
-                      className='w-full px-4 py-2 text-left focus:outline-none'
+                      className='w-full px-4 rounded-none py-2 text-left focus:outline-none'
                       onClick={() => handleSelectLocation(result)}
                     >
                       <p className='text-sm truncate'>{result.display_name}</p>
