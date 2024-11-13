bcf412fc66132fe2999e9b6a829cb7ed4b975b6f
egidiosalinaro <egy.s96@gmail.com>
2024-11-13 06:59:09 +0000

    13 nov 2024, 07:59

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -21,7 +21,7 @@ export default async function Layout({
         </div>
       </nav>
       <div className='flex flex-col gap-12 items-start'>{children}</div>
-      <footer className='w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16'>
+      <footer className='w-full flex items-center justify-center border-t mx-auto text-center text-xs pt-1'>
         <p>Powered by Attitude Group S.p.a.</p>
         <ThemeSwitcher />
       </footer>
