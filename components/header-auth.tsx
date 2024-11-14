6a4b7474981dd28ff5d823bd5c249b0e00319eaa
Egidio Salinaro <egy.s96@gmail.com>
2024-11-14 15:51:39 +0000

    Nov 14, 2024, 4:51 PM

--- components/header-auth.tsx
+++ components/header-auth.tsx
@@ -32,12 +32,14 @@ export default async function AuthButton() {
     <div className='flex items-center gap-4'>
       <Button variant='ghost' className='p-0 h-8 w-8 rounded-full'>
         <Avatar className='h-8 w-8 flex items-center justify-center'>
-          <List />
+          <List className='h-4 w-4' />
         </Avatar>
       </Button>
       <form action={signOutAction}>
-        <Button type='submit' variant={'outline'} className='rounded-full'>
-          <LogOut />
+        <Button variant='ghost' className='p-0 h-8 w-8 rounded-full'>
+          <Avatar className='h-8 w-8 flex items-center justify-center'>
+            <LogOut className='h-4 w-4' />
+          </Avatar>
         </Button>
       </form>
     </div>
