aa9443d14c0f93c23b97f2d1d52a52aae69fac3b
Egidio Salinaro <egy.s96@gmail.com>
2024-11-15 12:16:41 +0000

    Nov 15, 2024, 1:16 PM

--- components/StorePopup.tsx
+++ components/StorePopup.tsx
@@ -37,6 +37,7 @@ const StorePopup: React.FC<StorePopupProps> = ({
   handleSendEmail,
 }) => {
   const storeCoordinates = parseCoords(store.location);
+  console.log(store.location);
 
   return (
     <div>
